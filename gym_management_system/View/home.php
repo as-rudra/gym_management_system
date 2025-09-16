b6ce33ce32015ab085b00ffd15651e3a307a1fc8
anik <anik.inc@gmail.com>
2025-09-16 23:58:27 +0600

    updated

---
+++ gym_management_system/View/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Body Trend</title>
+        <link rel="stylesheet" href="CSS/home.css"> 
+    </head>
+    <body>
+        <header>
+            <div id="div_menu">                
+                <ul>  
+                    <li style="padding-top: 0px;"><img src="../Asset/BodyTrend.png" style="width: 180px; height: 100px;"></li>
+                    <li><a>HOME</a></li>
+                    <li><a>ABOUT</a></li>
+                    <li><a>GALLERY</a></li>
+                    <li><a>CONTACT US</a></li>
+                    <li style="padding-top: 20px"><a><button>Login</button></a></li>
+                </ul>                
+            </div>
+
+            <div class="img_container">
+                <img style= "width: 100%;" src="../Asset/Cover.png">
+                <p>
+                    <span style="color: chartreuse; font-size: 50px;">Train | Sweat | Grow</span><br><br>
+                    Every workout brings you one step closer to your goals<br>
+                    Stay focused, stay motivated, and push beyond your limits<br>
+                    Your fitness journey starts here - stronger, healthier, unstoppable
+                </p>
+                <a href="registration.php"><button class="btn">Register Now ></button></a>
+                
+            </div>
+        </header>
+
+        <footer>
+            <div id="footer_div">
+                <p>
+                    Have questions or need help? <br>
+                    Reachout to us at:   
+                </p>
+                <ul>
+                    <img src="../Asset/Facebook_icon.svg.png">
+                    <img style="padding-left: 30px" src="../Asset/Instagram_icon.png">
+                    <img style="padding-left: 30px; width: 50px; height: 40px;" src="../Asset/Gmail_icon_(2020).svg.png">
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ gym_management_system/View/registration.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registration page</title>
+        <link rel="stylesheet" href="CSS/registration.css">
+    </head>
+    <body>
+        <div id="registration_div">
+            <img src="../Asset/regiBackground.png" style="width: 100%; height: 100%;">
+            <div id="login_div">
+                <img src="../Asset/BodyTrend.png" style="width: 180px; height: 100px;">
+                <p style="margin-top: 22%; font-size: 35px;">Welcome back!</p>
+                <p>To keep connected with us please<br>login with your personal info</p>
+                <button>Log in</button>
+            </div>
+            <div id="regi_div">
+                <p style="margin-top: 10%; font-size: 35px;">Create Account</p>
+                <p>Fill-up the form<br>with your personal info</p>
+                <button>Sign up</button>
+            </div>
+        </div>
+    </body>
+</html>
